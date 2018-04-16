014f55e56243ec5ae376b8b42fa7e5ce260d1db8
a17javierov <a17javierov@iessanclemente.net>
2018-04-16 12:59:45 +0200

    Ejercicio javadoc A17javierOV Agregados comentarios Javadoc

--- src/proyecto_interes/Interes.java
+++ src/proyecto_interes/Interes.java
@@ -1,20 +1,64 @@
-/*
- * Cálculo de interés simple = capital * redito * tiempo/100.
- * El capital y el tiempo son >0.
- * El redito puede ser negativo o cero.
- * autor: profesor
- */
 package proyecto_interes;
+
+/*******************************************************************************
+ *
+ * @author A17JavierOV Ejercicio Javadoc
+ * 
+ * Class Interes:
+ * 
+ * Atributo capital El capital inicial sobre el que se calculará el interés
+ * Atributo redito El interés al que estará ese capital
+ * Atributo tiempo El tiempo que permanecerá el capital al interés determinado (años)
+ * 
+ * Constructor Interes(float capital, float redito, int tiempo)
+ * 
+ * Metodo CalcularInteres() devuelve un double = capital*redito*tiempo/100
+ * 
+ ******************************************************************************/
 public class Interes {
+
+    /***************************************************************************
+     *  Se declaran las variables de la clase:
+     *  float capital;
+     *  float redito;
+     *  float tiempo;
+     **************************************************************************/
+    
     public int x=2;
     private float capital;
     private float redito;
     private int tiempo;
+
+    /***************************************************************************
+     * Constructor: 
+     * Interes(float capital, float redito, int tiempo)
+     * Se declara el interés insertando un float capital, float
+     * redito y un int tiempo en años.
+     * 
+     * 
+     * @param capital El capital inicial sobre el que se calculará el interés
+     * @param redito El interés al que estará ese capital
+     * @param tiempo El tiempo que permanecerá el capital al interés determinado (años)
+     * 
+     **************************************************************************/
+    
     public Interes(float capital, float redito, int tiempo) {        
         this.capital=capital;
         this.redito=redito;
         this.tiempo=tiempo;
     }
+
+    /***************************************************************************
+     *
+     * @return Devuelve el interés dado un capital y un interés a lo largo de un
+     * tiempo: Capital * interés * tiempo /100
+     * 
+     * @throws Exception
+     *  El capital debe ser mayor que 0 y el tiempo debe ser mayor que 0.
+     * 
+     * 
+     **************************************************************************/
+    
     public double CalcularInteres()throws Exception{
         if (capital<=0)
             throw (new Exception ("Error. El capital tiene que ser >=0"));
--- src/proyecto_interes/Main.java
+++ src/proyecto_interes/Main.java
@@ -1,4 +1,4 @@
-/*****************************************************************************
+/******************************************************************************
  * Visualizar el interes simple que produce un capital (capital)
  * a un determinado tipo de interes (redito) anual expresado en tanto por ciento
  * y considerando el tiempo (tiempo) en años.
@@ -8,7 +8,39 @@
 
 package proyecto_interes;
 import java.util.*;
+
+/*******************************************************************************
+ *
+ * @author A17JavierOV Ejercicio Javadoc
+ * 
+ * Clase principal de la aplicación.
+ * Calcula el interés simple:
+ * 
+ *  1.- Teclea el capital que debe ser >0
+ *  2.- Teclea el rédito
+ *  3.- Teclea el tiempo en años y sin decimales
+ * 
+ * Devuelve:
+ * 
+ *  El interés simple calculado según el método Interes.CalcularInteres
+ * 
+ *  Si saltan excepciones imprimirá textos con los fallos.
+ *  Inserta los valores directamente a la clase Interes.
+ *  El main solo consta de una variable de clase Scanner y otra booleana "error"
+ * 
+ ******************************************************************************/
 public class Main {
+
+    /***************************************************************************
+     *
+     * @param args
+     * 
+     * Consta de 2 variables:
+     * Scanner teclado;
+     * boolean error;
+     * 
+     **************************************************************************/
+    
     public static void main(String[] args) {
         System.out.println("CÁLCULO DEL INTERÉS SIMPLE");
         Scanner teclado = new Scanner(System.in);
